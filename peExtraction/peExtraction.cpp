7275b9e3a06a6d6fb7d4b4a191e1fe4b90e6ebae
Huy (Valen) Võ <tienhuyvonguyen@gmail.com>
2022-12-19 18:04:59 +0700

    init

--- peExtraction/peExtraction.cpp
+++ peExtraction/peExtraction.cpp
@@ -1,52 +1,66 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
 void showMenu();
+std::string get_input_filename();
+std::string get_file_contents(std::string const& infile);
+int userInput();
+
 int main()
 {
 	std::cout << "Welcome to the PE Extraction Tool" << std::endl;
 	std::cout << "Please choose from the following options" << std::endl;
-	showMenu();
-	int choice = 0;
-	while (choice != -1)
+	std::string input_filename = get_input_filename();
+	std::string content = get_file_contents(input_filename);
+	std::cout << content << std::endl;
+	try
 	{
-		std::cout << "Enter your choice: ";
-		std::cin >> choice;
-		switch (choice)
+		showMenu();
+		int choice = userInput();
+		while (choice != -1)
 		{
-		case 1:
-			std::cout << "You chose 1" << std::endl;
-			break;
-		case 2:
-			std::cout << "You chose 2" << std::endl;
-			break;
-		case 3:
-			std::cout << "You chose 3" << std::endl;
-			break;
-		case 4:
-			std::cout << "You chose 4" << std::endl;
-			break;
-		case 5:
-			std::cout << "You chose 5" << std::endl;
-			break;
-		case 6:
-			std::cout << "You chose 6" << std::endl;
-			break;
-		case 7:
-			std::cout << "You chose 7" << std::endl;
-			break;
-		case 8:
-			std::cout << "You chose 8" << std::endl;
-			break;
-		case 9:
-			std::cout << "You chose 9" << std::endl;
-			break;
-		case 10:
-			std::cout << "You chose 10" << std::endl;
-			break;
-		default:
-			std::cout << "Invalid choice" << std::endl;
-			break;
+			switch (choice)
+			{
+			case 1:
+				std::cout << "You chose 1" << std::endl;
+				break;
+			case 2:
+				std::cout << "You chose 2" << std::endl;
+				break;
+			case 3:
+				std::cout << "You chose 3" << std::endl;
+				break;
+			case 4:
+				std::cout << "You chose 4" << std::endl;
+				break;
+			case 5:
+				std::cout << "You chose 5" << std::endl;
+				break;
+			case 6:
+				std::cout << "You chose 6" << std::endl;
+				break;
+			case 7:
+				std::cout << "You chose 7" << std::endl;
+				break;
+			case 8:
+				std::cout << "You chose 8" << std::endl;
+				break;
+			case 9:
+				std::cout << "You chose 9" << std::endl;
+				break;
+			case 10:
+				std::cout << "You chose 10" << std::endl;
+				break;
+			default:
+				std::cout << "Invalid choice" << std::endl;
+				break;
+			}
 		}
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Error" << std::endl;
 	}
 	std::cout << "Thank you for using the PE Extraction Tool" << std::endl
 		<< "Press any key to exit" << std::endl;
@@ -69,5 +83,32 @@ void showMenu()
 	std::cout << "8. Raw Size" << std::endl;
 	std::cout << "9. Virtual Address" << std::endl;
 	std::cout << "10. Virtual Size" << std::endl;
+}
 
+int userInput() {
+	int choice = 0;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
+}
+
+std::string get_file_contents(std::string const& infile)
+{
+	std::ifstream inf(infile);
+	std::string contents;
+	int c;
+	while ((c = inf.get()) != EOF)
+	{
+		contents += c;
+	}
+	return contents;
+}
+
+std::string get_input_filename()
+{
+	std::string filename;
+	std::cout << "Please enter the name of the input file." << std::endl;
+	std::cout << "Filename: ";
+	std::cin >> filename;
+	return filename;
 }
