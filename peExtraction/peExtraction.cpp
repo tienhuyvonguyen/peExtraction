e25f766e050cb47bd7b4cea75827c4ef35188660
Vader <tienhuyvonguyen@gmail.com>
2022-12-19 14:23:13 +0700

    Initialize

--- peExtraction/peExtraction.cpp
+++ peExtraction/peExtraction.cpp
@@ -1,20 +1,73 @@
-// peExtraction.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <string>
+void showMenu();
 int main()
 {
-
+	std::cout << "Welcome to the PE Extraction Tool" << std::endl;
+	std::cout << "Please choose from the following options" << std::endl;
+	showMenu();
+	int choice = 0;
+	while (choice != -1)
+	{
+		std::cout << "Enter your choice: ";
+		std::cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			std::cout << "You chose 1" << std::endl;
+			break;
+		case 2:
+			std::cout << "You chose 2" << std::endl;
+			break;
+		case 3:
+			std::cout << "You chose 3" << std::endl;
+			break;
+		case 4:
+			std::cout << "You chose 4" << std::endl;
+			break;
+		case 5:
+			std::cout << "You chose 5" << std::endl;
+			break;
+		case 6:
+			std::cout << "You chose 6" << std::endl;
+			break;
+		case 7:
+			std::cout << "You chose 7" << std::endl;
+			break;
+		case 8:
+			std::cout << "You chose 8" << std::endl;
+			break;
+		case 9:
+			std::cout << "You chose 9" << std::endl;
+			break;
+		case 10:
+			std::cout << "You chose 10" << std::endl;
+			break;
+		default:
+			std::cout << "Invalid choice" << std::endl;
+			break;
+		}
+	}
+	std::cout << "Thank you for using the PE Extraction Tool" << std::endl
+		<< "Press any key to exit" << std::endl;
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// show menu function 
+void showMenu()
+{
+	std::cout << "=== PE File Information ===" << std::endl;
+	std::cout << "1. Point to Entry Point" << std::endl;
+	std::cout << "2. CheckSum" << std::endl;
+	std::cout << "3. Imagebase" << std::endl;
+	std::cout << "4. File Aligment" << std::endl;
+	std::cout << "5. Size of image" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "=== PE File Section Information ===" << std::endl;
+	std::cout << "6. Characteristics" << std::endl;
+	std::cout << "7. Raw Address" << std::endl;
+	std::cout << "8. Raw Size" << std::endl;
+	std::cout << "9. Virtual Address" << std::endl;
+	std::cout << "10. Virtual Size" << std::endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
